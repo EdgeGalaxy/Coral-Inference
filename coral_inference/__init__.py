2cf122c9899c4c81a2bdff0e8d85647f53f341fe
zhaokefei <zhaokefei@tezign.com>
2024-11-26 10:19:15 +0800

    feat: set version

--- coral_inference/__init__.py
+++ coral_inference/__init__.py
@@ -1,11 +1,3 @@
 from inference import *
 
 from coral_inference.core import *
-from coral_inference.plugins import *
-
-
-__version__ = "0.0.2"
-
-
-if __name__ == "__main__":
-    print(__version__)
