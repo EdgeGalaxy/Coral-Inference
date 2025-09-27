282867a6a4ed4a175f7344f784309c8dbb61d7b7
zhaokefei <546109656@qq.com>
2025-09-27 22:18:30 +0800

    fix: add run_env

--- coral_inference/core/managers/patch_pingback.py
+++ coral_inference/core/managers/patch_pingback.py
@@ -3,11 +3,13 @@ from coral_inference.core.env import (
     INFLUXDB_METRICS_TOKEN, 
     INFLUXDB_METRICS_URL
 )
+from coral_inference.core.models.utils import get_runtime_platform
 
 
 def get_influxdb_metrics():
     return {
         'metrics_host': INFLUXDB_METRICS_URL,
         'metrics_token': INFLUXDB_METRICS_TOKEN,
-        'metrics_database': INFLUXDB_METRICS_DATABASE
+        'metrics_database': INFLUXDB_METRICS_DATABASE,
+        'run_env': get_runtime_platform()
     }
